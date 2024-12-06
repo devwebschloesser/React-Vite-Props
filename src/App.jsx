fc32f4b105ebcfa6da048edaef44ab5477700c18
Leonardo Schloesser <work_dev_schloesserp@outlook.com>
2024-12-06 00:13:16 -0300

    vite - props

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import Profile from "./Components/Profile";
+
+export default function App() {
+  return (
+    <div className="app">
+      <Profile
+        avatar="https://avatars.githubusercontent.com/u/182534244?…00&u=0675f8b8dd2df5bd0e8d7177526f142a7da157a7&v=4"
+        name="Leonardo Schloesser"
+        bio="Front-end web developer at SAP"
+        email="devwebschloesser@gmail.com"
+        phone="+55 (48) 98819-1263"
+        githubUrl="https://github.com/devwebschloesser"
+        linkedinUrl="https://www.linkedin.com/in/leonardo-schloesser-pinheiro-github-devwebschloesser-a0311b303/"
+        instagramUrl="https://www.instagram.com/_77catarina/"
+      />
+    </div>
+  );
+}
